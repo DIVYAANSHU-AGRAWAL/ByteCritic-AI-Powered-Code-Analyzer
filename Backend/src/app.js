9af71f39773f41e23f7dfdfc32e38d608a53c088
DIVYAANSHU AGRAWAL <divyaanshuagrawal2711@gmail.com>
2025-07-25 02:16:32 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,7 +4,12 @@ const cors = require ('cors')       // Required to connect backend to frontend
 
 const app = express();
 
-app.use(cors());                    // Must always be at the top of all middlewares
+app.use(cors({                                // Must always be at the top of all middlewares
+  origin: "https://bytecritic.onrender.com", // your frontend domain
+  methods: ["GET", "POST"],
+  credentials: true
+}));
+               
 app.use(express.json()); // Required for req.body
 app.use("/ai", aiRoutes);
 
